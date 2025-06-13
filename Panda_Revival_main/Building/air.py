8ea16dc137cdc0d6a9818a893e1e826db1c4cc58
MACCHA-co <133297445+MACCHA-co@users.noreply.github.com>
2025-06-13 11:58:20 +0900

    Add files via upload

---
+++ Panda_Revival_main/Building/air.py
@@ -0,0 +1,10 @@
+from gdpc import Editor, Block, Transform, geometry
+
+
+def air(editor,x,y,z,q_id):
+    for xx in range(34):
+        for yy in range(10):
+            for zz in range(36):
+                editor.placeBlock((x-17+xx,y-1+yy,z-16+zz),Block(q_id))
+def Air(editor,x,y,z):
+    air(editor,x,y-20,z,"air")
